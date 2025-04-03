aa2f464f6b7e958e1f1d9f1b8f1e8d63b7d59420
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-04-03 10:15:44 -0400

    feat(app): remove cra code and add logic for Codecademy project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+const people = [
+  "Elvis Presley",
+  "Jimmy Buffett",
+  "Johnny Depp",
+  "Kevin James",
+  "Babe Ruth",
+  "Kobe Bryant",
+  "Shaquille O'Neal",
+  "Tony Stark",
+];
+const activities = [
+  "watched a movie ",
+  "started a podcast ",
+  "walked a dog ",
+  "learned React ",
+  "went for a run ",
+  "pet a cat ",
+  "kissed a frog ",
+  "read a book ",
+];
+const places = [
+  " the beach",
+  " Disney World",
+  " the pizzaria",
+  " the golf course",
+  " a restaurant",
+  " your mom's house",
+  " the bowling alley",
+  " the park",
+];
+const finalMsg = [];
+const randomPerson = Math.floor(Math.random() * people.length);
+const randomAcivity = Math.floor(Math.random() * activities.length);
+const randomPlace = Math.floor(Math.random() * places.length);
+finalMsg.push(people[randomPerson]);
+finalMsg.push(" " + activities[randomAcivity]);
+finalMsg.push("at " + places[randomPlace]);
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{finalMsg}</header>
     </div>
   );
 }
